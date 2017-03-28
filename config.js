c2f395271b553a2d28ed64f06abac4cd30e1ddca
moks <silviu.ovidiu.iancu@gmail.com>
2017-03-28 23:32:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
   // Your bot's user token. If you don't know what that is, go here:
   // https://discordapp.com/developers/applications/me
   // Then create a new application and grab your token.
-  token: "Mjc3NDY4MjUxMTk2MzU4NjU4.C3eqbg.1elXpC2bpowh4Jrju4nfdMCbvSE",
+  token: "insert token here",
 
   // If this option is enabled, the bot will delete the message that triggered it, and its own
   // response, after the specified amount of time has passed.
